6a2678e2db9898529fa3bc9a7c34652f10c4eb83
Jason <jason5donoghue@gmail.com>
2023-09-13 20:33:06 +0100

    added extra subjects

--- gradesList.py
+++ gradesList.py
@@ -1,4 +1,8 @@
 subjects = ["physics", "calculus", "poetry", "history"]
 grades = [98, 97, 85, 88]
 gradebook = [[subjects[0], grades[0]], [subjects[1], grades[1]],[subjects[2], grades[2]], [subjects[3], grades[3]]]
+
+gradebook.append(["computer science", 100])
+gradebook.append(["visual arts", 93])
+
 print(gradebook)
