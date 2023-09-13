135a9dfeb479b4ca2e0c1406f94624b85eab56ec
Jason <jason5donoghue@gmail.com>
2023-09-13 20:39:46 +0100

    complete entire gradebook

--- gradesList.py
+++ gradesList.py
@@ -8,4 +8,7 @@ gradebook.append(["visual arts", 93])
 gradebook[5][1] += 5
 gradebook[2].remove(85)
 gradebook[2].append("Pass")
-print(gradebook)
+
+last_semester_gradebook = [["politics", 80], ["latin", 96], ["dance", 97], ["architecture", 65]]
+full_gradebook = gradebook + last_semester_gradebook
+print(full_gradebook)
